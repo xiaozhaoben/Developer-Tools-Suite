aa4d77f5a52eb765febd80aeb8b7da69c6c07967
hzt <2502712438@qq.com>
2025-07-04 11:40:21 +0800

    FIX GitgHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/YOUR_REPOSITORY_NAME/', // 替换为你的仓库名
+  base: '/Nginx-Log-Parser/', // 使用你的实际仓库名
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
